e08729fbf699b2c7a2a2fb7c0567d716d25f8a19
rohitchaoji <53764185+rohitchaoji@users.noreply.github.com>
2024-02-28 16:51:23 +0530

    Added generic level_script file
    to replace individual level scripts as they all did the exact same thing

---
+++ JumperClone/src/Levels/level_script.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+const powerup_scene = preload("res://src/Objects/extra_jump.tscn")
+@export var next_scene: PackedScene
+
+func restart_level():
+	get_tree().reload_current_scene()
+
+func respawn_powerup(pos: Vector2):
+	var timer = get_tree().create_timer(2.0)
+	await timer.timeout
+	var new_powerup = powerup_scene.instantiate()
+	new_powerup.position = pos
+	add_child(new_powerup)
+
+func next_level():
+	if next_scene != null:
+		get_tree().change_scene_to_packed(next_scene)
+
+func trigger_platforms():
+	if $JumpTriggerPlatforms != null:
+		var platforms = $JumpTriggerPlatforms.get_children()
+		for platform in platforms:
+			platform.movement()
